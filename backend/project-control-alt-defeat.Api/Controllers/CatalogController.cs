36e5f5d99f5055d68bd041ce91bd5fff287aae31
chasenewton98 <52009086+chasenewton98@users.noreply.github.com>
2021-04-08 16:04:51 -0400

    finished?

--- backend/project-control-alt-defeat.Api/Controllers/CatalogController.cs
+++ backend/project-control-alt-defeat.Api/Controllers/CatalogController.cs
@@ -32,5 +32,28 @@ namespace project.control.alt.defeat.Api.Controllers
         {
             return Created("/catalog/42", item);
         }
+
+        [HttpPost("{id:int}/ratings")]
+        public IActionResult PostRating(int id, [FromBody] Rating rating)
+        {
+
+            var item = new Item("Shirt","Ohio State shirt.", "Nike",29.99m);
+            item.Id = id;
+            item.AddRating(rating);
+
+            return Ok(item);
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult PutItem(int id, [FromBody] Item item)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteItem(int id)
+        {
+            return Ok();
+        }
     }
 }
