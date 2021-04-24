e595c8ce33834e1b627b47ce674b414eb64ebf8b
CadeSantha <cadesantha@gmail.com>
2021-04-24 14:16:23 -0400

    Added to solution file, did build, and initial test

---
+++ backend/project-control-alt-defeat.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace project_control_alt_defeat.Domain.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
